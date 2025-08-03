2e1e3851fdc05bdb7e52d9f68168c37956171e14
Alex Sh <shalex.work@gmail.com>
2025-08-03 10:35:03 +0300

    Add isp file

--- reset_isp.sh
+++ reset_isp.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-cd /var/nvidia/nvcam/settings/
-sudo rm -rf nvcam_* serial_*
+ISP_DIR="/var/nvidia/nvcam/settings"
 
+# Copy the camera overrides file to the ISP directory
+sudo cp camera_overrides.isp "$ISP_DIR"/camera_overrides.isp
+sudo chown root:root "$ISP_DIR"/camera_overrides.isp
+
+# Reset the ISP settings
+cd "$ISP_DIR"
+sudo rm -rf nvcam_* serial_*
 sudo systemctl restart nvargus-daemon
